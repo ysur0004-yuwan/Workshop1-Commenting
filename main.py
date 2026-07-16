9007bb8153fca55b3a3b43dcb32baa3631b05248
Callum Atkinson <callum.atkinson@monash.edu>
2026-07-16 16:52:05 +1000

    Update main.py
    remove comments

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ from averager import average_images
 
 if __name__ == "__main__":
     output_dir = "generated_images"
-
-    # Step 1: Generate noisy images
     generate_noisy_images(output_dir, n_images=30)
-
-    # Step 2: Average them to remove noise
     average_images(output_dir, output_path="denoised.png")
